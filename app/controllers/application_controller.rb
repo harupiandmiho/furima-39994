65c1fcd34ff65a8fa45745e8a78b2243348493da
harupiandmiho <152187842+harupiandmiho@users.noreply.github.com>
2024-02-24 14:37:02 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
       username == 'admin' && password == '2222'
+    end
   end
 end
