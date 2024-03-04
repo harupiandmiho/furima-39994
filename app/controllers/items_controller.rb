b77d454e29b8b87539924953b8878e5d69df18b8
harupiandmiho <152187842+harupiandmiho@users.noreply.github.com>
2024-03-04 23:46:04 +0900

    商品出品ページを表示できるようにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @item = Item.new
+  end
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path # 保存に成功した場合はトップページへリダイレクト
+    else
+      render :new # 保存に失敗した場合は出品ページを再表示
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, :price, :image)
+    # 上記のpermit内のシンボルは、フォームで扱う各入力項目のname属性に対応しています
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,19 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :shipping_area
+  belongs_to_active_hash :days_to_ship
+
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 0 }
+  validates :condition_id, presence: true, numericality: { other_than: 0 }
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 0 }
+  validates :shipping_area_id, presence: true, numericality: { other_than: 0 }
+  validates :days_to_ship_id, presence: true, numericality: { other_than: 0 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
