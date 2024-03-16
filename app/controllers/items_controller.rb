c50c67caa44deba8271048beb92c2e95ebef8ade
harupiandmiho <152187842+harupiandmiho@users.noreply.github.com>
2024-03-16 16:58:14 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,12 +24,7 @@ class ItemsController < ApplicationController
     # set_itemメソッドで@itemを設定
   end
 
-  def edit
-    @item = Item.find(params[:id])
-  end
-
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
