4f2323003bb85a2a9ee4112d06db06947bb0cedf
harupiandmiho <152187842+harupiandmiho@users.noreply.github.com>
2024-03-27 16:08:04 +0900

    Update 20240317040218_create_shipping_addresses.rb

--- db/migrate/20240317040218_create_shipping_addresses.rb
+++ db/migrate/20240317040218_create_shipping_addresses.rb
@@ -2,7 +2,7 @@ class CreateShippingAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :shipping_addresses do |t|
       t.string :postal_code
-      t.integer :prefecture_id
+      t.integer :shipping_area_id
       t.string :city
       t.string :address
       t.string :building_name
